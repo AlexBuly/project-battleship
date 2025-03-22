3288aa6579bef0291b4cf8a4004de6e61e8fd6e1
Alex Buly <asbuly@icloud.com>
2025-03-22 16:18:51 -0400

    Adds functionality for placing ships

--- script.js
+++ script.js
@@ -23,9 +23,68 @@ function Ship(length) {
     return ship
 }
 
-const Gameboard = () => {
+function Gameboard () {
+    const gridCells = [];
+    const rows = 10;
+    const columns = 10;
     
+    for (let i = 0; i < rows; i++) {
+      gridCells[i] = [];
+      for (let j = 0; j < columns; j++) {
+        gridCells[i][j] = [];
+      }
+    }
+           
+    const gameboard = () => {
+        console.log(gridCells); 
+    }
+
+    const placeShip = (rowStart, colStart, direction) => {
+        if (direction === "right") {
+            if (colStart <= 7 && rowStart <= 9) {
+                let i = 0;
+                while (i < 3) {
+                    i++;
+                    gridCells[rowStart][colStart++] = "X";
+                }
+            } 
+        } else if (direction === "down") {
+            if (rowStart <= 7 && colStart <= 9) {
+                let i = 0;
+                while (i < 3) {
+                    i++;
+                    gridCells[rowStart++][colStart] = "X";
+                }
+            }
+        } else if (direction === "left") {
+            if (colStart <= 9 && colStart >= 2 && rowStart <= 9) {
+                let i = 0;
+                while (i < 3) {
+                    i++;
+                    gridCells[rowStart][colStart--] = "X";
+                }
+
+            }
+        } else if (direction === "up") {
+            if (rowStart <= 9 && rowStart >= 2 && colStart <= 9) {
+                let i = 0;
+                while (i < 3) {
+                    i++;
+                    gridCells[rowStart--][colStart] = "X";
+                }
+            }
+        } 
+    }
+
+
+
+    return {gameboard, placeShip}
 }
+
+const gameboard = Gameboard();
+gameboard.placeShip(0, 3, "right");
+gameboard.gameboard();
+
 
 class Player {
     // player
