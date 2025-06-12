96ae50bb5d3a3d565ddfac12bbf68452c4221d35
Alex Buly <asbuly@icloud.com>
2025-06-12 09:21:19 -0400

    Update tests

--- script.test.js
+++ script.test.js
@@ -7,16 +7,16 @@ const gameboard = Gameboard();
 const cruiser = ship.addShip("cruiser", 3);
 
 test("Test ship details", () => {
-    expect(cruiser.shipDetails()).toBe("name: cruiser, length: 3, hits: 0, sunk: false");
+    expect(cruiser.shipDetails()).toBe("hits: 0 sunk: false");
 });
 
 test("Test ship hits" ,() => {
     cruiser.hit();
-    expect(cruiser.shipDetails()).toBe("name: cruiser, length: 3, hits: 1, sunk: false");
+    expect(cruiser.shipDetails()).toBe("hits: 1 sunk: false");
 });
 
 test("Test ship sunk", () => {
     cruiser.hit();
     cruiser.hit();
-    expect(cruiser.shipDetails()).toBe("name: cruiser, length: 3, hits: 3, sunk: true");
+    expect(cruiser.shipDetails()).toBe("hits: 3 sunk: true");
 });
